0e5a573b5b0ffc888dca1a774a8f4c9b0618ad8d
femelo22 <luiz123jfmg@gmail.com>
2023-10-03 20:44:21 -0300

    feat: start project

---
+++ src/main/java/br/com/lf/BatchWebserviceReaderApplication.java
@@ -0,0 +1,13 @@
+package br.com.lf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchWebserviceReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchWebserviceReaderApplication.class, args);
+	}
+
+}
