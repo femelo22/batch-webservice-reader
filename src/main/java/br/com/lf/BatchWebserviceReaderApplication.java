c8be3f95a6961a52aa2f1cbb36e55ee43b60317c
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-10-13 14:45:24 -0300

    feat: read pageable users

--- src/main/java/br/com/lf/BatchWebserviceReaderApplication.java
+++ src/main/java/br/com/lf/BatchWebserviceReaderApplication.java
@@ -2,12 +2,15 @@ package br.com.lf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BatchWebserviceReaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchWebserviceReaderApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BatchWebserviceReaderApplication.class, args);
+		context.close();
 	}
 
 }
--- src/main/java/br/com/lf/reader/UserReaderConfig.java
+++ src/main/java/br/com/lf/reader/UserReaderConfig.java
@@ -2,14 +2,19 @@ package br.com.lf.reader;
 
 import br.com.lf.domain.ResponseUser;
 import br.com.lf.domain.User;
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.BeforeChunk;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -17,13 +22,31 @@ public class UserReaderConfig implements ItemReader<User> {
 
     private RestTemplate restTemplate = new RestTemplate();
     private int page = 1;
+    private List<User> users = new ArrayList<>();
+    private int userIndex = 0;
+    private int total = 0;
+
+    @Value("${chunkSize}")
+    private int chunkSize;
+
+    @Value("${pageSize}")
+    private int pageSize;
+
+    @Value("${limit}")
+    private int limit;
 
     @Override
     public User read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        User user;
 
-        fetchUserDataFromAPI();
+        if(userIndex < users.size())
+            user = users.get(userIndex);
+        else
+            user = null;
 
-        return null;
+        userIndex++;
+
+        return user;
     }
 
     private List<User> fetchUserDataFromAPI() {
@@ -33,4 +56,24 @@ public class UserReaderConfig implements ItemReader<User> {
         List<User> userData = response.getBody().getData();
         return  userData;
     }
+
+    @BeforeChunk
+    private void beforeChunk(ChunkContext context) {
+        for(int i = 0; i < chunkSize; i += pageSize) {
+            if(total >= limit) return;
+
+            users.addAll(fetchUserDataFromAPI());
+            total += users.size();
+            page++;
+        }
+    }
+
+    @AfterChunk
+    private void afterChunk(ChunkContext context) {
+        System.out.println("Fim do Chunk");
+        userIndex = 0;
+        users = new ArrayList<>();
+    }
+
+
 }
